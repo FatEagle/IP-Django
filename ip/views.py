021db7c430c8e93eeee415c282b65c400529e58b
LiChang <lancegentry@qq.com>
2019-03-31 11:49:22 +0800

    test

--- ip/views.py
+++ ip/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 class IPView(View):
 
     def get(self, request):
+        ret2 = request.META['REMOTE_ADDR']
         ret = request.META['HTTP_X_FORWARDED_FOR']
-        return HttpResponse(ret)
+        return HttpResponse(ret, ret2)
 
