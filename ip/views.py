90211ba54b1d9ef46bb37b95b66032391eebb5c5
LiChang <lancegentry@qq.com>
2019-04-05 13:32:51 +0800

    add enter

--- ip/views.py
+++ ip/views.py
@@ -34,7 +34,7 @@ class IPView(View):
         address = json.load(address)
 
         return HttpResponse(
-            "您的IP是(Your IP is): {}\n地理位置是: 省:{}, 市:{} ".format(
+            "<p>您的IP是(Your IP is): {}<\p><p>地理位置是: 省:{}, 市:{}<\p>".format(
                 ret,
                 address['content']['address_detail']['province'],
                 address['content']['address_detail']['city'])
