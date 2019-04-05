11bdaee26a2619829c3dd8221a450ea979ae9f65
LiChang <lancegentry@qq.com>
2019-04-05 17:40:58 +0800

    重构

--- README.md
+++ README.md
@@ -1,46 +1,13 @@
-# 挖优笔记API - DEMO
+# 用被反向代理后的Django获取IP地址和地理信息
 
+## 获取IP
+反向代理后, 用户IP不会存储在`REMOTE_ADDR`中, 而会存储在`HTTP_X_FORWARDED_FOR`.
 
-## 搜索
-使用Elasticsearch作为后端进行搜索, 具体实现是让想要被检索的对象使用继承了SearchSerializer的Serializer. 这样在对象进行创建和更新时, SearchSerializer会调用to_representation()在这里进行对Elasticsearch的文档的创建与删除.
+`REMOTE_ADDR` 存储了NGINX发出的请求时的IP.
+`HTTP_X_FORWARDED_FOR` 存储了访问NGINX的请求的IP
 
-### Elasticsearch的初始化配置
-需要对每一个需要存储在Elasticsearch的类创建一个index, 并对index做mapping, 以使得文档支持中文搜索.
+## 获取地理位置信息
+使用百度地图开放地址平台, 官方网址如下:
+http://lbsyun.baidu.com/index.php?title=%E9%A6%96%E9%A1%B5
 
-#### Mapping的模板
-==拼音分词器有bug==, 不适用于长文本
-
-Tokenizer使用了 IK Analysis对文本进行中文分词, 其有两种模式:
-* ik_smart: 粗粒度
-* ik_max_word: 细粒度
-
-Token filter使用了pinyin, 将词转换为拼音全拼和首字母,如:
-    信息 --> xinxi, xx
-
-以下模板是先对文本进行ik_max_word模式的中文分词
-```
-PUT IndexName
-{
-    "index": {
-        "analysis": {
-            "analyzer": {
-                "ik_pinyin_analyzer": {
-                    "type": "custom",
-                    "tokenizer": "ik_max_word",
-                    "filter": ["my_pinyin", "word_delimiter"]
-                }
-            },
-            "filter": {
-                "my_pinyin": {
-                    "type" : "pinyin",
-                    "keep_full_pinyin" : false,
-                    "keep_joined_full_pinyin": true,
-                    "keep_none_chinese": true,
-                    "keep_none_chinese_together": true,
-                    "none_chinese_pinyin_tokenize": false
-                }
-            }
-        }
-    }
-}
-```
+其可以通过IP地址获取地理位置, 调用百度的API即可.
--- ip/views.py
+++ ip/views.py
@@ -1,7 +1,7 @@
 import ssl
 import json
+# 防止冲突
 from urllib import request as urllib_request
-from urllib import parse
 
 from django.views import View
 from django.http import HttpResponse
@@ -11,32 +11,40 @@ from django.conf import settings
 class IPView(View):
 
     def get(self, request):
-        # 反向代理IP
+        ip = self.__get_user_ip(request)
+        address = self.__get_address_by_baidu_api(ip)
+
+        return HttpResponse(
+            "<p>您的IP是(Your IP is): {}</p><p>地理位置是: 省:{}, 市:{}</p>".format(
+                ip,
+                address['content']['address_detail']['province'],
+                address['content']['address_detail']['city'])
+        )
+
+    @staticmethod
+    def __get_user_ip(request):
         try:
-            ret2 = request.META['REMOTE_ADDR']
+            request_ip = request.META['REMOTE_ADDR']
         except KeyError:
             pass
 
         try:
-            # 真正的IP
-            ret = request.META['HTTP_X_FORWARDED_FOR']
+            # 反向代理后存储的IP
+            user_ip = request.META['HTTP_X_FORWARDED_FOR']
         except KeyError:
-            print('local')
-            ret = ret2
+            # 局域网请求
+            user_ip = None
 
+        return user_ip or request_ip
+
+    @staticmethod
+    def __get_address_by_baidu_api(ip):
         context = ssl._create_unverified_context()
         baidu_api = 'https://api.map.baidu.com/location/ip?ip={ip}&ak={ak}&coor=bd09ll'.format(
-            ip=ret,
+            ip=ip,
             ak=settings.AK
         )
 
         address = urllib_request.urlopen(baidu_api, context=context)
-        address = json.load(address)
-
-        return HttpResponse(
-            "<p>您的IP是(Your IP is): {}</p><p>地理位置是: 省:{}, 市:{}</p>".format(
-                ret,
-                address['content']['address_detail']['province'],
-                address['content']['address_detail']['city'])
-        )
+        return json.load(address)
 
